d1a566d83b9a90ba6a538b425c227b7a26a36001
Kratika Singh <kratikaarun@gmail.com>
2025-05-28 08:02:49 +0530

    Admin Dashboard Done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Route,Routes } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
+import Login from './components/Login'
+
 const App = () => {
+
+  const [token,setToken]=useState('');
+
+
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <>
+
+      { token ===""
+      ? <Login/>:<>
         <Navbar/>
         <hr />
         <div className='flex w-full'>
@@ -22,7 +31,8 @@ const App = () => {
           </div>
         </div>
       
-      </>
+      </> }
+      
     </div>
     
   )
