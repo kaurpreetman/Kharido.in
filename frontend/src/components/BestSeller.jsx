f9891907c6e6f09bf56b68911dfc032db6c8cad1
Kratika Singh <kratikaarun@gmail.com>
2025-06-07 11:01:07 +0530

    best seller page finalized

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+import { ProductCard } from './ui/ProductCard';
+
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext)
+  const [bestSeller, setBestSeller] = useState([])
+
+  useEffect(() => {
+    const bestProduct = products.filter((item) => item.bestseller)
+    setBestSeller(bestProduct.slice(0, 5))
+  }, [products])
+
+  return (
+    <div className='my-10'>
+      <div className='text-center text-3xl py-8'>
+        <Title text1={'BEST'} text2={'SELLERS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Discover our top-selling products, loved by customers for their quality, value, and style. Handpicked favorites that you don't want to miss!
+        </p>
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {bestSeller.map((item, index) => (
+          <ProductCard key={index} product={item} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BestSeller
